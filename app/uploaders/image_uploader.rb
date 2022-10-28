77bcb28696fedd815d5cd21df5ad19c40e136cd2
vishal Thakur <vish75810@gmail.com>
2022-10-28 20:00:51 +0530

    first commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,15 +29,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+     process resize_to_fit: [400, 300]
+   end
+
+   version :default do
+    process resize_to_fit: [800, 600]
+   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_allowlist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
